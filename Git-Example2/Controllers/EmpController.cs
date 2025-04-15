016cb363cf844d625fb64c5b905708cfd171b348
Rashmi R S <rashmirangana.surpu@hcl.com>
2025-04-15 12:39:18 +0530

    Nranch1- Emp controller added

---
+++ Git-Example2/Controllers/EmpController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Git_Example2.Controllers
+{
+    public class EmpController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
