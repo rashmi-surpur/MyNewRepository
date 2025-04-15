2837393c14f462559b4e178f714292ea616c11b3
Rashmi R S <rashmirangana.surpu@hcl.com>
2025-04-15 12:27:29 +0530

    Student Controller added , Page1 added

---
+++ Git-Example2/Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Git_Example2.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Page1()
+        {
+            return View();
+        }
+    }
+}
